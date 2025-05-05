496406671af96c8fd8fdc6a341e42c5fba647240
Dias <Maxatovdias@gmail.com>
2025-05-05 21:48:02 +0500

    Update urls.py

--- nfacplay/urls.py
+++ nfacplay/urls.py
@@ -18,7 +18,23 @@ Including another URLconf
 from django.urls import path,include
 import os
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="API documentation for my project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@myapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    path('',include('One.urls'))
+    path('',include('One.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
 ]
