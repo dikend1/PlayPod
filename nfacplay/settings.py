8febb0e83dad70b1ac2e87170ba802db9ba7780e
dikend1 <maxatovdias@gmail.com>
2025-05-05 21:22:39 +0500

    update

--- nfacplay/settings.py
+++ nfacplay/settings.py
@@ -24,16 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-#env = environ.Env()
-# Чтение .env файла, если он есть
-import environ
-
-env = environ.Env()
-# Чтение .env файла, если он есть
-environ.Env.read_env()
-
-SECRET_KEY = env('SECRET_KEY')  # Чтение секретного ключа из переменной окружения
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
